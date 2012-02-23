c70063c243104fb49e8bb1071e9eb19b49340380
hvelarde <hector.velarde@gmail.com>
2012-02-23 15:53:51 -0200

    i18ndude must run now regardless you are using the development buildout or you have your own

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
+PATH=bin:$PATH
 I18NDOMAIN="collective.prettydate"
+BASE_DIRECTORY="src/collective/prettydate"
 
 # Synchronise the templates and scripts with the .pot.
-# All on one line normally:
-i18ndude rebuild-pot --pot src/collective/prettydate/locales/${I18NDOMAIN}.pot \
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
     --create ${I18NDOMAIN} \
-   src/collective/prettydate
+    ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
-for po in src/collective/prettydate/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    i18ndude sync --pot src/collective/prettydate/locales/${I18NDOMAIN}.pot $po
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
+
+# Report of errors and suspect untranslated messages
+i18ndude find-untranslated ${BASE_DIRECTORY}
